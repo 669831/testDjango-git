2ac18dcbd8efffd17b60a5477223574c2399429a
mario <mstroo87@outlook.com>
2023-04-02 14:18:31 +0200

    opmerking over taal in settings.py toegevoegd

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
-
+# hierdoor bepaalt u de taal van uw applicatie
 LANGUAGE_CODE = 'nl-nl'
 
 TIME_ZONE = 'CET'
